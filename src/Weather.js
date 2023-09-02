0fe77ce70edd37a74ba0d9d30c520a71f7ccb5cc
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-09-02 12:01:27 -0500

    set template for conditional rendering

--- src/Weather.js
+++ src/Weather.js
@@ -4,10 +4,11 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
- const key = "82f43b0671f2tb328187o7be4ab620aa";
- let [currentDetails, setCurrentDetails] = useState(null);
  let [city, setCity] = useState("");
- let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+ let [currentDetails, setCurrentDetails] = useState(null);
+ let [done, setDone] = useState(false);
+
+ let key = "82f43b0671f2tb328187o7be4ab620aa";
 
  function changeCity(event) {
   setCity(event.target.value);
@@ -15,7 +16,9 @@ export default function Weather() {
 
  function getCurrent(event) {
   event.preventDefault();
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
   axios.get(url).then(displayCurrent);
+  setDone(true);
  }
 
  function displayCurrent(response) {
@@ -28,38 +31,42 @@ export default function Weather() {
   );
  }
 
- return (
-  <div className="Weather">
-   <div className="main-body">
-    <form className="text-center" onSubmit={getCurrent}>
-     <input type="text" placeholder="Enter City Here" onChange={changeCity} />
-     <input type="submit" className="btn btn-outline-light ms-1" />
-    </form>
-    <h1>
-     <div className="row justify-content-evenly">
-      <div className="col top-upper-left">
-       <div className="city">Tokyo</div>
-       <div className="date-time fs-3 fw-normal">Tuesday 3:02</div>
-       <section>{currentDetails}</section>
+ if (done) {
+  return (
+   <div className="Weather">
+    <div className="main-body">
+     <form className="text-center" onSubmit={getCurrent}>
+      <input type="text" placeholder="Enter City Here" onChange={changeCity} />
+      <input type="submit" className="btn btn-outline-light ms-1" />
+     </form>
+     <h1>
+      <div className="row justify-content-evenly">
+       <div className="col top-upper-left">
+        <div className="city">Tokyo</div>
+        <div className="date-time fs-3 fw-normal">Tuesday 3:02</div>
+        <section>{currentDetails}</section>
+       </div>
+       <div className="col top-upper-right">
+        <p className="current-emoji">🌧️</p>
+        <span className="current-temperature">19</span>
+        <span className="unit-convert">
+         {" "}
+         <a href="/" id="celsiusLink" className="active">
+          °C
+         </a>{" "}
+         |{" "}
+         <a href="/" id="fahrenheitLink">
+          °F
+         </a>
+        </span>
+       </div>
+       <div className="five-day"></div>
       </div>
-      <div className="col top-upper-right">
-       <p className="current-emoji">🌧️</p>
-       <span className="current-temperature">19</span>
-       <span className="unit-convert">
-        {" "}
-        <a href="/" id="celsiusLink" className="active">
-         °C
-        </a>{" "}
-        |{" "}
-        <a href="/" id="fahrenheitLink">
-         °F
-        </a>
-       </span>
-      </div>
-      <div className="five-day"></div>
-     </div>
-    </h1>
+     </h1>
+    </div>
    </div>
-  </div>
- );
+  );
+ } else {
+  return <p>Loading...</p>;
+ }
 }
