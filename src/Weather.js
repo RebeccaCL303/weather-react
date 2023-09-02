043c2ae759429a76f2b3218d428f065df0b1c640
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-09-02 13:23:57 -0500

    changed weather data into an object

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,7 @@ import "./Weather.css";
 
 export default function Weather() {
  let [city, setCity] = useState("");
- let [currentDetails, setCurrentDetails] = useState(null);
+ let [weatherData, setWeatherData] = useState({ done: false });
  let [done, setDone] = useState(false);
 
  function changeCity(event) {
@@ -16,20 +16,21 @@ export default function Weather() {
   event.preventDefault();
   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=82f43b0671f2tb328187o7be4ab620aa&units=metric`;
   axios.get(url).then(displayCurrent);
-  setDone(true);
  }
 
  function displayCurrent(response) {
-  setCurrentDetails(
-   <ul>
-    <li>{response.data.condition.description}</li>
-    <li>Humidity: {response.data.temperature.humidity}%</li>
-    <li>Wind: {response.data.wind.speed}km/h</li>
-   </ul>
-  );
+  setWeatherData({
+   done: true,
+   city: `${response.data.city}`,
+   description: `${response.data.condition.description}`,
+   humidity: `${response.data.temperature.humidity}%`,
+   wind: `${response.data.wind.speed}km/h`,
+   temperature: Math.round(response.data.temperature.current),
+   icon: `${response.data.condition.icon_url}`,
+  });
  }
 
- if (done) {
+ if (weatherData.done) {
   return (
    <div className="Weather">
     <div className="main-body">
@@ -44,13 +45,17 @@ export default function Weather() {
      <h1>
       <div className="row justify-content-evenly">
        <div className="col upper-left">
-        <div>Tokyo</div>
+        <div>{weatherData.city}</div>
         <div className="fs-3 fw-normal">Tuesday 3:02</div>
-        <section>{currentDetails}</section>
+        <ul>
+         <li className="text-capitalize">{weatherData.description}</li>
+         <li>Humidity: {weatherData.humidity}</li>
+         <li>Wind: {weatherData.wind}</li>
+        </ul>
        </div>
        <div className="col upper-right">
-        <p>🌧️</p>
-        <span className="current-temperature">19</span>
+        <span>🌧️</span>
+        <span className="current-temperature">{weatherData.temperature}</span>
         <span className="unit-convert">
          {" "}
          <a href="/" className="active">
