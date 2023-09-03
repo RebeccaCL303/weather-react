64c85415905b1fdc942c848c7253468741a2abd9
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-09-03 14:40:45 -0500

    styled forecast day, changed color scheme

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Icon from "./Icon.js";
+
+import "./Forecast.css";
 
 export default function Forecast() {
- return <div className="Forecast">forecast goes here</div>;
+ return (
+  <div className="Forecast">
+   <div className="row">
+    <div className="col">
+     <div className="day">Mon</div>
+     <div>
+      <Icon code="clear-sky-night" />
+      <div className="high">12°</div>
+      <div className="low">10°</div>
+     </div>
+    </div>
+   </div>
+  </div>
+ );
 }
