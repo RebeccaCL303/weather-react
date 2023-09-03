c42abe376a1d12e1ca4f16dda46a893e9ca35b7a
BEX-0 <126423309+BEX-0@users.noreply.github.com>
2023-09-03 12:37:48 -0500

    added functions to convert between metric and imperial

--- src/TempConvert.js
+++ src/TempConvert.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TempConvert(props) {
- return (
-  <span className="TempConvert">
-   <span className="current-temperature">{props.celsius}</span>
-   <span className="unit-convert">
-    {" "}
-    <a href="/" className="active">
-     °C
-    </a>{" "}
-    | <a href="/">°F</a>
+ let [unit, setUnit] = useState("metric");
+ let fahrenheitTemp = Math.round((props.celsius * 9) / 5 + 32);
+
+ function convertToFahrenheit(event) {
+  event.preventDefault();
+  setUnit("imperial");
+ }
+
+ function convertToCelsius(event) {
+  event.preventDefault();
+  setUnit("metric");
+ }
+
+ function handleClick(event) {
+  event.preventDefault();
+ }
+
+ if (unit === "metric") {
+  return (
+   <span className="TempConvert">
+    <span className="current-temperature">{props.celsius}</span>
+    <span className="unit-convert">
+     {" "}
+     <a href="/" className="active" onClick={handleClick}>
+      °C
+     </a>{" "}
+     |{" "}
+     <a onClick={convertToFahrenheit} href="/">
+      °F
+     </a>
+    </span>
    </span>
-  </span>
- );
+  );
+ } else {
+  return (
+   <span className="TempConvert">
+    <span className="current-temperature">{fahrenheitTemp}</span>
+    <span className="unit-convert">
+     {" "}
+     <a href="/" onClick={convertToCelsius}>
+      °C
+     </a>{" "}
+     |{" "}
+     <a className="active" href="/" onClick={handleClick}>
+      °F
+     </a>
+    </span>
+   </span>
+  );
+ }
 }
